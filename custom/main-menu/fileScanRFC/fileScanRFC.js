0ea0277c3c962fd3009decd05011fe31d024cca7
Salvador Lopez <salvador.lopez@enterconecta.com>
2020-06-12 15:26:55 -0500

    Validación de archivo seleccionado, solo imágenes png jpg y jpeg y se aumenta timeout de petición hacia api

--- custom/main-menu/fileScanRFC/fileScanRFC.js
+++ custom/main-menu/fileScanRFC/fileScanRFC.js
@@ -92,133 +92,165 @@ let ScanQRView = customization.extend(NomadView, {
     },
 
     sendRequestRFC(event){
-        app.alert.show('getInfoRFC', {
+        //Obtener extensión del archivo elegido solo aceptar (jpg,jpeg y png);
+        var extension=self.getFileExt($('#file_rfc')[0].files[0]);
+        if(extension != 'png' && extension != 'jpg' && extension != 'jpeg'){
+
+            app.alert.show('error_extension', {
+                level: 'error',
+                messages: 'Archivo no soportado, favor de elegir imagen con extensión jpg, jpeg o png',
+                autoClose: true
+            });
+            $('#send_request').addClass('disabled');
+            $('#send_request').css('pointer-events','none');
+        }else{
+            app.alert.show('getInfoRFC', {
                 level: 'load',
                 closeable: false,
                 messages: app.lang.get('LBL_LOADING'),
-        });
+            });
 
-        $('#send_request').addClass('disabled');
-        $('#send_request').css('pointer-events','none');
+            $('#send_request').addClass('disabled');
+            $('#send_request').css('pointer-events','none');
 
-        //var base_64_str=self.getBase64Image(document.getElementById('imageRFC_QR'));
-        var c = document.createElement('canvas');
-        var ctx = c.getContext('2d');
-        var imgn = new Image;
-        var imageData='';
-        imgn.src = URL.createObjectURL($('#file_rfc')[0].files[0]);
-        imgn.onload = function () {
-            c.width = imgn.width;
-            c.height = imgn.height;
-            ctx.drawImage(imgn, 0, 0);
-            var imageData = c.toDataURL('image/png');
-            var body={
-                "file":imageData
-            }
+            //var base_64_str=self.getBase64Image(document.getElementById('imageRFC_QR'));
+            var c = document.createElement('canvas');
+            var ctx = c.getContext('2d');
+            var imgn = new Image;
+            var imageData='';
+            imgn.src = URL.createObjectURL($('#file_rfc')[0].files[0]);
+            imgn.onload = function () {
+                c.width = imgn.width;
+                c.height = imgn.height;
+                ctx.drawImage(imgn, 0, 0);
+                var imageData = c.toDataURL('image/png');
+                var body={
+                    "file":$('#imageRFC_QR').attr('src')
+                }
 
-            app.api.call('create', app.api.buildURL("GetInfoRFCbyQR"), body, {
-                success: _.bind(function (data) {
-                    app.alert.dismiss('getInfoRFC');
-                    if(data !=null){
-                        if (data[0]['Mensaje de error']!=undefined) {
-                            
+                app.api.call('create', app.api.buildURL("GetInfoRFCbyQR"), body, {
+                    success: _.bind(function (data) {
+                        app.alert.dismiss('getInfoRFC');
+                        if(data !=null){
+                            if (data[0]['Mensaje de error']!=undefined) {
+                                
+                                app.alert.show('error_rfc', {
+                                    level: 'error',
+                                    messages: data[0]['Mensaje de error'],
+                                    autoClose: true
+                                    }
+                                );
+                                //Habilitando nuevamente el botón de enviar
+                                $('#send_request').removeClass('disabled');
+                                $('#send_request').css('pointer-events','auto');
+                            }else{
+                                var contextoQR=self;
+                                //Comprobar que el RFC no existe para poder crear o actualizar el registro
+                                 app.alert.show('validando_duplicados', {
+                                    level: 'load',
+                                    messages: 'Cargando...'
+                                });
+                                 contextoQR.data=data;
+
+                                 var urlCuentas= app.api.buildURL("Accounts/", null, null, {
+                                    fields: "rfc_c",
+                                    max_num: 5,
+                                    "filter": [
+                                    {"rfc_c": data[0]['RFC']}
+                                    ]
+                                });
+                                 app.api.call("read", urlCuentas , null, {
+                                    success: _.bind(function (data) {
+                                        app.alert.dismiss('validando_duplicados');
+                                        //Se encontró que RFC ya se encuentra dado de alta
+                                        if (data.records.length > 0) {
+
+                                            dialog.showConfirm('EL RFC que se está intentando obtener ya se encuentra en el sistema\n¿Desea Consultarlo?', {
+                                                callback: function(index) {
+                                                    if (index === 2) {//Aceptar
+                                                        app.controller.navigate({
+                                                            url: 'Accounts/'+data.records[0].id
+                                                        });  
+                                                    }
+                                                }
+                                            });
+                                            
+                                        }else{
+                                            //No se encontró rfc, por lo tanto, el proceso sigue para crear o actualizar Cuenta
+                                            app.alert.show('success_rfc', {
+                                                level: 'success',
+                                                messages: 'Información cargada correctamente',
+                                                autoClose: true
+                                            }
+                                            );
+
+                                            if(self.desdeCualVista.from=='creacion'){
+                                                dialog.showAlert('Será redirigido a la creación de la Cuenta', {
+                                                    title: 'Información cargada correctamente',
+                                                    buttonLabels: 'Aceptar'
+                                                });
+
+                                                app.controller.navigate({
+                                                    url: 'Accounts/create',
+                                                    data:{
+                                                        dataFromQR:contextoQR.data[0]
+                                                    }
+                                                });
+                                            }else{
+
+                                                dialog.showAlert('Será redirigido a la actualización de la Cuenta', {
+                                                    title: 'Información cargada correctamente',
+                                                    buttonLabels: 'Aceptar'
+                                                });
+
+                                                app.controller.navigate({
+                                                    url: 'Accounts/'+self.desdeCualVista.idCuenta+'/edit',
+                                                    data:{
+                                                        dataFromQR:contextoQR.data[0],
+                                                        vista:'edit'
+                                                    }
+                                                });
+
+                                            }
+                                        }
+                                    }, self)
+                                });
+                            }
+                        }else{
                             app.alert.show('error_rfc', {
-                                level: 'error',
-                                messages: data[0]['Mensaje de error'],
-                                autoClose: true
-                                }
+                                    level: 'error',
+                                    messages: 'La información no se procesó correctamente, favor de elegir otra imagen',
+                                    autoClose: true
+                                    }
                             );
-                            //Habilitando nuevamente el botón de enviar
                             $('#send_request').removeClass('disabled');
                             $('#send_request').css('pointer-events','auto');
-                        }else{
-                            var contextoQR=self;
-                            //Comprobar que el RFC no existe para poder crear o actualizar el registro
-                             app.alert.show('validando_duplicados', {
-                                level: 'load',
-                                messages: 'Cargando...'
-                            });
-                             contextoQR.data=data;
 
-                             var urlCuentas= app.api.buildURL("Accounts/", null, null, {
-                                fields: "rfc_c",
-                                max_num: 5,
-                                "filter": [
-                                {"rfc_c": data[0]['RFC']}
-                                ]
-                            });
-                             app.api.call("read", urlCuentas , null, {
-                                success: _.bind(function (data) {
-                                    app.alert.dismiss('validando_duplicados');
-                                    //Se encontró que RFC ya se encuentra dado de alta
-                                    if (data.records.length > 0) {
-
-                                        dialog.showConfirm('EL RFC que se está intentando obtener ya se encuentra en el sistema\n¿Desea Consultarlo?', {
-                                            callback: function(index) {
-                                                if (index === 2) {//Aceptar
-                                                    app.controller.navigate({
-                                                        url: 'Accounts/'+data.records[0].id
-                                                    });  
-                                                }
-                                            }
-                                        });
-                                        
-                                    }else{
-                                        //No se encontró rfc, por lo tanto, el proceso sigue para crear o actualizar Cuenta
-                                        app.alert.show('success_rfc', {
-                                            level: 'success',
-                                            messages: 'Información cargada correctamente',
-                                            autoClose: true
-                                        }
-                                        );
-
-                                        if(self.desdeCualVista.from=='creacion'){
-                                            dialog.showAlert('Será redirigido a la creación de la Cuenta', {
-                                                title: 'Información cargada correctamente',
-                                                buttonLabels: 'Aceptar'
-                                            });
-
-                                            app.controller.navigate({
-                                                url: 'Accounts/create',
-                                                data:{
-                                                    dataFromQR:contextoQR.data[0]
-                                                }
-                                            });
-                                        }else{
-
-                                            dialog.showAlert('Será redirigido a la actualización de la Cuenta', {
-                                                title: 'Información cargada correctamente',
-                                                buttonLabels: 'Aceptar'
-                                            });
-
-                                            app.controller.navigate({
-                                                url: 'Accounts/'+self.desdeCualVista.idCuenta+'/edit',
-                                                data:{
-                                                    dataFromQR:contextoQR.data[0],
-                                                    vista:'edit'
-                                                }
-                                            });
-
-                                        }
-                                    }
-                                }, self)
-                            });
-                        }
-                    }else{
+                        }  
+                    }, self),
+                    error: _.bind(function (response) {
+                        app.alert.dismiss('getInfoRFC');
                         app.alert.show('error_rfc', {
-                                level: 'error',
-                                messages: 'La información no se procesó correctamente, favor de elegir otra imagen',
-                                autoClose: true
-                                }
-                        );
+                                    level: 'error',
+                                    messages: response.textStatus+'\nLa imagen es muy pesada, favor de elegir una nueva: ',
+                                    autoClose: true
+                                    }
+                            );
                         $('#send_request').removeClass('disabled');
                         $('#send_request').css('pointer-events','auto');
-
-                    }  
-                }, self),
-            });
+                    },self)
+                },{timeout:60000});
+            }
         }
+    },
 
+    getFileExt(campo) {
+        var name=campo.name;
+        var lastDot = name.lastIndexOf('.');
+        var fileName = name.substring(0, lastDot);
+        var ext = name.substring(lastDot + 1);
+
+        return ext;
     },
 
     openDialogFile(e){
